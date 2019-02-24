c03b2fce1f1226678e3a0a7cf13b873be0dff487
Magnus <magnus@bitsplit.io>
2019-02-24 03:30:37 +0100

    adding lab1 template. graphics displaying code

---
+++ time4mips/buffer.c
@@ -0,0 +1,45 @@
+#include <stdint.h> 
+
+uint8_t megabuffer[128*32]; 
+uint8_t displaybuffer[128*4];
+
+
+void clear() {
+	int r;
+	for(r = 0; r < 128*32; r++)
+		megabuffer[r] = 1;
+}
+
+void buffer_make(int x, int y, uint8_t *sprite){
+	int width = 16;
+	int height = 16;
+
+	int sy;
+	int sx;
+	for(sy = 0; sy < height; sy++ ){		
+		for(sx = 0; sx < width; sx++){
+		 megabuffer[(sy+y)*128 + x+sx] = *(sprite+(sy*width + sx));
+		}
+	}
+}
+
+uint8_t * toscreenbuffer(){
+	int index = 0;
+	int y;
+	int x;
+	int j;
+for (y = 0; y < 4; y++) {
+		for(x = 0; x < 128; x++) {
+			uint8_t sum = 0;
+			for(j = 0; j < 8; j++) {
+				sum += ((uint8_t)megabuffer[((y*8) + j)*128 + x] << j);				  
+			}	
+		displaybuffer[index] = sum;
+		index++;
+		}
+	}
+	return &displaybuffer;
+}
+
+
+
---
+++ time4mips/buffer.h
@@ -0,0 +1,2 @@
+void buffer_make(int x, int y, uint8_t *sprite);
+uint8_t * toscreenbuffer();
